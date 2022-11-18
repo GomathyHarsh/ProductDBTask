fded0eb34323fd6b538fcf61f2bc28edb40898d6
Gomathy <110896778+GomathyHarsh@users.noreply.github.com>
2022-11-18 15:29:56 +0530

    updated

--- query.js
+++ query.js
@@ -12,14 +12,14 @@ db.products.find({$and:[{$or:[{product_price:{$lt:400}},{product_price:{$gt:600}
 db.products.find({product_price:{$gt:500}});
 
 //5 Find the product name and product material of each products
-db.products.find({product_name:1,product_material:1});
+db.products.find({id:"10"},{product_name:1,product_material:1});
 
 //6 Find the product with a row id of 10
 db.products.find( {"id":{$eq: "10"}});
 
 
 //7 Find only the product name and product material
-
+db.products.find({id:"10"},{product_name:1,product_material:1});
 
 //8 Find all products which contain the value of soft in product material 
 db.products.find({"product_material":{$eq:"soft"}});
@@ -31,3 +31,6 @@ db.products.find( {
         { $or: [ { product_color: { $eq : "indigo" } }, { product_price : { $eq: 492.00 } } ] }]});
 
 //10 Delete the products which product price value are same
+db.products.aggregate([{$group:{_id:"$product_price",count:{$sum:1}}},{$match:{count:{$gt:1}}}]).forEach((doc)=>{
+    db.products.remove({product_price:doc._id});
+})
